abbdb3871dbeae72ff1e683759cf8962e3c63132
Md Mahabubul  Alam Abir <abrarnawaralfy@gmail.com>
2026-08-29 23:26:24 +0600

    Add multiplication function

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from datetime import date
-from utils import add, subtract
+from utils import add, subtract, multiply
 
 print("Name: Md. Abrar Nawar")
 print("Today's date:", date.today())
 
 print("5 + 3 =", add(5, 3))
 print("5 - 3 =", subtract(5, 3))
+print("5 * 3 =", multiply(5, 3))
