c6e2887b9abf0430da9e6722509ea46467cde4ca
Md Mahabubul  Alam Abir <abrarnawaralfy@gmail.com>
2026-08-29 23:40:50 +0600

    Add calculator error handling

--- src/main.py
+++ src/main.py
@@ -7,4 +7,7 @@ print("Today's date:", date.today())
 print("5 + 3 =", add(5, 3))
 print("5 - 3 =", subtract(5, 3))
 print("5 * 3 =", multiply(5, 3))
-print("6 / 3 =", divide(6, 3))
+try:
+    print("6 / 0 =", divide(6, 0))
+except ValueError as error:
+    print("Error:", error)
