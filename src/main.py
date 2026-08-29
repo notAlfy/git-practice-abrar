f649fe1645c5c6ad6c2538524984f66fd054095b
Md Mahabubul  Alam Abir <abrarnawaralfy@gmail.com>
2026-08-29 23:33:52 +0600

    Add division function

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from datetime import date
-from utils import add, subtract, multiply
+from utils import add, subtract, multiply, divide
 
 print("Name: Md. Abrar Nawar")
 print("Today's date:", date.today())
@@ -7,3 +7,4 @@ print("Today's date:", date.today())
 print("5 + 3 =", add(5, 3))
 print("5 - 3 =", subtract(5, 3))
 print("5 * 3 =", multiply(5, 3))
+print("6 / 3 =", divide(6, 3))
